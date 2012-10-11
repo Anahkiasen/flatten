d8c9ba1bcc5bc2bcc17aa511ce378fe49f127ade
Anahkiasen <ehtnam6@gmail.com>
2012-10-11 09:48:45 +0200

    Cleanup the config file

--- config/flatten.php
+++ config/flatten.php
@@ -1,25 +1,20 @@
 <?php return array(
 
+  // The environments in which Flatten should not run
+  'environments' => array('local'),
+
   // The folder inside storage/cache where the pages will be stored
-  'folder' => 'pages',
+  'folder'       => 'pages',
 
   // The Laravel Flatten will start caching from
-  'hook' => 'laravel.started: application',
+  'hook'         => 'laravel.started: application',
 
   // The different pages to ignored when caching
   // They're all regexes so go crazy
-  'ignore' => array(
-  ),
+  'ignore'       => array(),
 
   // List only specific pages to cache, useful if you have a lot of
   // pages you don't want to see cached
   // The ignored pages will still be substracted from this array
-  'only' => array(
-  ),
-
-  // The environments in which Flatten should not run
-  'environments' => array(
-    'test',
-  ),
-
+  'only'         => array(),
 );
