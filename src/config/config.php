d5696edac11f3f566452b98aa6b3be1f7c23ae2a
Maxime Fabre <ehtnam6@gmail.com>
2013-08-18 16:42:48 +0200

    Realign config options

--- src/config/config.php
+++ src/config/config.php
@@ -5,7 +5,7 @@
 
 	// The default period during which a cached page should be kept (in minutes)
 	// 0 means the page never gets refreshed by itself
-	'lifetime' => 0,
+	'lifetime'     => 0,
 
 	// The different pages to be ignored when caching
 	// They're all regexes so go crazy
@@ -18,5 +18,5 @@
 
 	// An array of string or variables to add to the salt being used
 	// to differentiate pages
-	'saltshaker' => array(),
+	'saltshaker'   => array(),
 );
